bb76b1216f4138be09dbebe84387c2f19e8b4d2b
govindbansal1309 <80391508+govindbansal1309@users.noreply.github.com>
2021-04-30 09:57:54 +0530

    Update A1Header.h

--- inc/A1Header.h
+++ inc/A1Header.h
@@ -1,7 +1,13 @@
 #ifndef A1HEADER_H_INCLUDED
 #define A1HEADER_H_INCLUDED
-
+/* Brief
+* Sets the direction of the ports for the Buttons
+*/
 void PortSet();
+
+/* Brief
+* Gives output to LED if both the buttons are in 'close' position.
+*/
 void Buttons();
 
 
